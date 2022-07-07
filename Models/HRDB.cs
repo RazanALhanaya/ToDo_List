21262144630dd2db27b44848cdf5f919c92167b2
Razan <50563220+RazanALhanaya@users.noreply.github.com>
2022-07-07 17:06:44 +0300

    Models

---
+++ Models/HRDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todoList.Models;
+
+namespace todoList.Models
+{
+    public class HRDB : DbContext // inhert
+    {
+        public DbSet<List> Lists { get; set; } // any instances from Lists and data type is Dbset 
+
+        // will overrad fun. from DbContext 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Connection
+            optionsBuilder.UseSqlServer(@"data source=razan\sqlexpress01; initial catalog=List ; integrated security=SSPI;");
+        }
+
+    }
+}
---
+++ Models/List.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace todoList.Models
+{
+    [Table("list")]
+    public class List
+    {
+        //attributes 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Task number")]
+        public int ListId { get; set; }
+
+
+        //attributes  
+        [Required]
+        [Column(TypeName="varchar(50)")]
+        [Display(Name = "Task")]
+        public string Task { get; set; }
+
+
+        //attributes  
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        [Display(Name = "Date")]
+        public String DateOfTask { get; set; } 
+
+
+        [DataType(DataType.Time)]
+        [Column(TypeName = "varchar(10)")]
+        [Display(Name = "Time")]
+        public TimeSpan TimeOfTask { get; set; }
+    }
+}
