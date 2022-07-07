c7236d75913181646de6ad7836fc54784da67fd5
Razan <50563220+RazanALhanaya@users.noreply.github.com>
2022-07-07 17:05:25 +0300

    Migrations

---
+++ Migrations/20220705012900_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace todoList.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "list",
+                columns: table => new
+                {
+                    ListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Task = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DateOfTask = table.Column<string>(type: "varchar(10)", nullable: false),
+                    TimeOfTask = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_list", x => x.ListId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "list");
+        }
+    }
+}
