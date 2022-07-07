2fd677bd8c96750a29df4cf8bb037e8b3b700cfc
Razan <50563220+RazanALhanaya@users.noreply.github.com>
2022-07-07 16:43:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=List}/{action=Index}/{id?}");
+
+#pragma warning restore MVC1005 // Cannot use UseMvc with Endpoint Routing
+
+app.Run();
